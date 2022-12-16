39552d9c0520d9e1b56f8a610617258791c3782e
Lavesh Satim <lavesh.git@gmail.com>
2022-12-16 21:05:49 +0530

    spring version 2.7.6 && java 11 added in pom.xml

--- src/main/java/com/mycompany/propertymanagement/controller/PropertyController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/PropertyController.java
@@ -1,14 +1,22 @@
 package com.mycompany.propertymanagement.controller;
 
-import com.mycompany.propertymanagement.dto.PropertyDTO;
-import com.mycompany.propertymanagement.service.PropertyService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.service.PropertyService;
 
 @RestController
 @RequestMapping("/api/v1")
--- src/main/java/com/mycompany/propertymanagement/entity/PropertyEntity.java
+++ src/main/java/com/mycompany/propertymanagement/entity/PropertyEntity.java
@@ -1,10 +1,15 @@
 package com.mycompany.propertymanagement.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
 
 @Entity
 @Table(name = "PROPERTY_TABLE")
@@ -13,15 +18,15 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class PropertyEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    @Column(name = "PROPERTY_TITLE", nullable = false)
-    private String title;
-    private String description;
-    private String ownerName;
-    @Column(name = "EMAIL")
-    private String ownerEmail;
-    private Double price;
-    private String address;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(name = "PROPERTY_TITLE", nullable = false)
+	private String title;
+	private String description;
+	private String ownerName;
+	@Column(name = "EMAIL")
+	private String ownerEmail;
+	private Double price;
+	private String address;
 }
